3df7019a79a04cfd4088af988ed6c60a43534296
MrWlobo <michaldworniczak40@gmail.com>
2026-01-02 15:55:23 +0100

    Added base for grid and priority queue.

---
+++ priority_queue.py
@@ -0,0 +1,95 @@
+class PriorityQueue:
+    def __init__(self, array: list[int], is_max_heap: bool):
+        self.heap = array
+        self.is_max_heap = is_max_heap
+        self.build_heap()
+
+    def in_order(self, parent: int, child: int):
+        if self.is_max_heap:
+            return True if parent >= child else False
+        else:
+            return True if parent <= child else False
+
+    def get_parent(self, index: int) -> int | None:
+        if not self.heap or index <= 0:
+            return None
+
+        return (index - 1) // 2
+
+    def get_left_child(self, index: int) -> int | None:
+        left_child_index = 2 * index + 1
+        if left_child_index < len(self.heap):
+            return left_child_index
+        else:
+            return None
+
+    def get_right_child(self, index: int) -> int | None:
+        right_child_index = 2 * index + 2
+        if right_child_index < len(self.heap):
+            return right_child_index
+        else:
+            return None
+
+    def heapify_up(self, index: int) -> None:
+        current_index = index
+        parent_index = self.get_parent(index)
+        valid_indices = parent_index is not None and current_index is not None
+
+        while valid_indices and not self.in_order(self.heap[parent_index], self.heap[current_index]):
+            self.heap[parent_index], self.heap[current_index] = self.heap[current_index], self.heap[parent_index]
+
+            current_index = parent_index
+            parent_index = self.get_parent(current_index)
+            valid_indices = parent_index is not None and current_index is not None
+
+    def heapify_down(self, index: int) -> None:
+        current_index = index
+        left_child_index, right_child_index = self.get_left_child(index), self.get_right_child(index)
+
+        if left_child_index is None:
+            return
+        if right_child_index is None:
+            child_index = left_child_index
+        else:
+            if self.in_order(self.heap[left_child_index], self.heap[right_child_index]):
+                child_index = left_child_index
+            else:
+                child_index = right_child_index
+
+        valid_indices = current_index is not None and child_index is not None
+
+        while valid_indices and not self.in_order(self.heap[current_index], self.heap[child_index]):
+            self.heap[current_index], self.heap[child_index] = self.heap[child_index], self.heap[current_index]
+
+            current_index = child_index
+            left_child_index, right_child_index = self.get_left_child(current_index), self.get_right_child(current_index)
+
+            if left_child_index is None:
+                return
+            if right_child_index is None:
+                child_index = left_child_index
+            else:
+                if self.in_order(self.heap[left_child_index], self.heap[right_child_index]):
+                    child_index = left_child_index
+                else:
+                    child_index = right_child_index
+
+            valid_indices = current_index is not None and child_index is not None
+
+    def build_heap(self):
+        for index in reversed(range(len(self.heap) // 2)):
+            self.heapify_down(index)
+
+    def get_root_value(self) -> int:
+        return self.heap[0]
+
+    def insert(self, value: int) -> None:
+        self.heap.append(value)
+        self.heapify_up(len(self.heap) - 1)
+
+    def remove(self):
+        self.heap[0] = self.heap.pop()
+        self.heapify_down(0)
+
+    def __repr__(self):
+        return str(self.heap)
